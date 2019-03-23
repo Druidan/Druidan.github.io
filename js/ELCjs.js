58009f9e7444d41824817701ff54e2f469124960
Druidan <44009123+Druidan@users.noreply.github.com>
2019-03-23 15:30:47 -0500

    added some responsive tweaks, and made at least the About Me and Portfolio Buttons Work.

--- js/ELCjs.js
+++ js/ELCjs.js
@@ -2,60 +2,111 @@ document.addEventListener("DOMContentLoaded", function() {
     //My JS starts past this point.
 
 //Global Button Grab
-const homeMenuBtn = document.getElementsByClassName("fa-home");
-const featuresMenuBtn = document.getElementsByClassName("fa-angle-double-down");
-const portfolioBtn = document.getElementsByClassName("portfolioNavItem");
-const aboutBtn = document.getElementsByClassName("aboutNavItem");
-const linkedInBtn = document.getElementsByClassName("fa-linkedin");
-const gitHubBtn = document.getElementsByClassName("fa-github");
-const twitterBtn = document.getElementsByClassName("fa-twitter");
-const faceBookBtn = document.getElementsByClassName("fa-facebook-square");
-const resumeDownloadBtn = document.getElementsByClassName("downloadResume");
-const contactBtn = document.getElementsByClassName("contactMeItem");
-const contactSubmitBtn = document.getElementsByClassName("");
-const vgWordGuessGameBtn = document.getElementsByClassName("vgWGPic");
-const swCombatGameBtn = document.getElementsByClassName("swCombat");
-const whaleTriviaBtn = document.getElementsByClassName("whaleTrivia");
-const rpsMultiBtn = document.getElementsByClassName("rpsPic");
+const homeMenuBtn = document.querySelectorAll(".fa-home");
+const featuresMenuBtn = document.querySelectorAll(".fa-angle-double-down");
+const introSection = document.querySelectorAll(".introSection");
+const portfolioBtn = document.querySelectorAll(".portfolioNavItem");
+const portfolioHeader = document.querySelectorAll(".portfolioHeader");
+const aboutBtn = document.querySelectorAll(".aboutNavItem");
+const aboutSection = document.querySelectorAll(".aboutMeSection");
+const linkedInBtn = document.querySelectorAll(".fa-linkedin");
+const gitHubBtn = document.querySelectorAll(".fa-github");
+const twitterBtn = document.querySelectorAll(".fa-twitter");
+const faceBookBtn = document.querySelectorAll(".fa-facebook-square");
+const resumeDownloadBtn = document.querySelectorAll(".downloadResume");
+// const contactBtn = document.querySelectorAll(".contactMeItem");
+const contactSection = document.querySelectorAll(".contactMeSection");
+// const contactSubmitBtn = document.querySelectorAll("");
+const vgWordGuessGameBtn = document.querySelectorAll(".vgWGPic");
+const swCombatGameBtn = document.querySelectorAll(".swCombat");
+const whaleTriviaBtn = document.querySelectorAll(".whaleTrivia");
+const rpsMultiBtn = document.querySelectorAll(".rpsPic");
 const allPortfolioItems = document.querySelectorAll(".portfolioItemWrapper");
 
-[].forEach.call(portfolioBtn,function(elm){
+portfolioBtn.forEach(function(elm){ 
     elm.addEventListener('click', function(){
-    for(let i = 0; i <allPortfolioItems.length; i++){ //Dear god vanilla JS is so much more complicated for the small things. I got this from Milind Anantwar at Stack Overflow - Source: "https://stackoverflow.com/questions/32586594/how-to-add-a-class-to-multiple-elements"
-        allPortfolioItems[i].classList.add("pImgAnimate");
-    }
+        portfolioHeader.forEach(function(pSection){
+            if(hasClass(pSection, "buryIt")){
+                removeClass(pSection, "buryIt")
+            } else{
+                addClass(pSection, "buryIt");
+            }
+        });
+        allPortfolioItems.forEach( function(item){ 
+            if(!hasClass(item, "pImgAnimate")){
+                addClass(item, "pImgAnimate")
+            } else{                        
+                removeClass(item, "pImgAnimate")}
+        });
+        introSection.forEach(function(iSection){
+            if(!hasClass(iSection, "buryIt")){
+                addClass(iSection, "buryIt")
+            }
+        });
+        aboutSection.forEach(function(aSection){
+            if(!hasClass(aSection, "buryIt")){
+                addClass(aSection, "buryIt")
+            }
+        });
+        contactSection.forEach(function(cSection){
+            if(!hasClass(cSection, "buryIt")){
+                addClass(cSection, "buryIt")
+            }
+        });
     }, false);
 });
 
-[].forEach.call(homeMenuBtn,function(elm){
+homeMenuBtn.forEach(function(elm){ 
     elm.addEventListener('click', function(){
         //Do a thing
     }, false);
 });
 
-[].forEach.call(featuresMenuBtn,function(elm){
+featuresMenuBtn.forEach(function(elm){ 
     elm.addEventListener('click', function(){
         //Do a thing
     }, false);
 });
 
-[].forEach.call(aboutBtn,function(elm){
+aboutBtn.forEach(function(elm){ 
     elm.addEventListener('click', function(){
-        //Do a thing
+        //if about section has the buryIt class, remove the buryIt class.
+        aboutSection.forEach(function(aSection){
+            if(hasClass(aSection, "buryIt")){
+                removeClass(aSection, "buryIt")
+            } else{
+                addClass(aSection, "buryIt");
+            }
+        });
+        introSection.forEach(function(iSection){
+            if(!hasClass(iSection, "buryIt")){
+                addClass(iSection, "buryIt")
+            }
+        });
+        portfolioHeader.forEach(function(pSection){
+            if(!hasClass(pSection, "buryIt")){
+                addClass(pSection, "buryIt")
+                allPortfolioItems.forEach( function(item){ 
+                    removeClass(item, "pImgAnimate")
+                });
+            }
+        });
+        //if contact section does not have the bury it class, add the bury it class.
+        //if profile section does not have buryit add bury it, and then remove animated class from each portfolio item.
     }, false);
 });
 
-[].forEach.call(contactBtn,function(elm){
-    elm.addEventListener('click', function(){
-        //Do a thing
-    }, false);
-});
+// contactBtn.forEach(function(elm){ 
+//     elm.addEventListener('click', function(){
+//         //Do a thing
+//     }, false);
+// });
 
-[].forEach.call(contactSubmitBtn,function(elm){
-    elm.addEventListener('click', function(){
-        //Do a thing
-    }, false);
-});
+// contactSubmitBtn.forEach(function(elm){ 
+//     elm.addEventListener('click', function(){
+//         //Do a thing
+//     }, false);
+// });
 
 //This beautiful little series of functions essentially creates functions that add or remove classes by pluging in the right arguments. I found this sweet puppy which was made by Jake Trent at "https://jaketrent.com/post/addremove-classes-raw-javascript/". -- UPDATE: And Now I might not use it because it works for IDs but not classes, as far as I can tell :(   
 function hasClass(el, className) {
