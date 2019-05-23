d9a539ae5ee38dd00764296e3489a511ca429158
Druidan <44009123+Druidan@users.noreply.github.com>
2019-05-22 21:26:08 -0500

    Adding the portfolio js with all of the basic info for each of the projects and homework in my portfolio.

---
+++ js/portfolio.js
@@ -0,0 +1,66 @@
+const portfolio = {
+    1: {
+        title: 'Video Game Hangman',
+        pic: './assets/images/TVwide.png',
+        deployed: 'https://druidan.github.io/Word-Guess-Game/index.html',
+        repo: 'https://github.com/Druidan/Word-Guess-Game', 
+        og: null, //For group projects, this link goes to the original repo before I refactored it.
+    },
+    2:  {
+        title: 'Star Wars Combat',
+        pic: './assets/images/SWLogo.png',
+        deployed: 'https://druidan.github.io/unit-4-game/index.html',
+        repo: 'https://github.com/Druidan/unit-4-game.git', 
+        og: null, //For group projects, this link goes to the original repo before I refactored it.
+    },
+    3: {
+        title: 'Whale Diplomacy',
+        pic: './assets/images/orcaCircles.gif',
+        deployed: 'https://druidan.github.io/TriviaGame/index.html',
+        repo: 'https://github.com/Druidan/TriviaGame.git', 
+        og: null, //For group projects, this link goes to the original repo before I refactored it.
+    },
+    4: {
+        title: 'Rock, Paper, Scissors Online',
+        pic: './assets/images/RPS.png',
+        deployed: 'https://druidan.github.io/RPS-Multiplayer/index.html',
+        repo: 'https://github.com/Druidan/RPS-Multiplayer.git', 
+        og: null, //For group projects, this link goes to the original repo before I refactored it.
+    },
+    5: {
+        title: 'Jeopardy Training',
+        pic: './assets/images/JeopardyLogo.jpg',
+        deployed: 'https://druidan.github.io/project1/index.html',
+        repo: 'https://github.com/Druidan/project1.git', 
+        og: 'https://github.com/Druidan/project1.git', //For group projects, this link goes to the original repo before I refactored it.
+    },
+    6: {
+        title: 'Liribot',
+        pic: './assets/images/LiriBot.png',
+        deployed: null,
+        repo: 'https://github.com/Druidan/liri-node-app.git', 
+        og: null, //For group projects, this link goes to the original repo before I refactored it.
+    },
+    7: {
+        title: 'Nerd Friend Finder',
+        pic: null, //MAKE IMG ASAP
+        deployed: 'https://gentle-fortress-32924.herokuapp.com/',
+        repo: 'https://github.com/Druidan/NerdFinder', 
+        og: null, //For group projects, this link goes to the original repo before I refactored it.
+    },
+    8: {
+        title: 'Burger Muncher',
+        pic: null, //MAKE IMG ASAP
+        deployed: 'https://gentle-hamlet-22873.herokuapp.com/',
+        repo: 'https://github.com/Druidan/burger-muncher', 
+        og: null, //For group projects, this link goes to the original repo before I refactored it.
+    },
+
+    9: {
+        title: 'Burger Battle!',
+        pic: null, //MAKE AN IMG ASAP
+        deployed: 'https://burgerbattle.herokuapp.com/',
+        repo: 'https://github.com/Druidan/project-2', 
+        og: null, //For group projects, this link goes to the original repo before I refactored it.
+    },
+}
