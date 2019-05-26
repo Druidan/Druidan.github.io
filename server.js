cceab261c0de54f0b02c8f8198ac40e7417c75a7
Druidan <44009123+Druidan@users.noreply.github.com>
2019-05-26 18:30:21 -0500

    Contact form is working! Now trying to make it so I don't get spammed and the sender knows something has been sent.

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.get('/', (req, res) => {
 const stork = nodemailer.createTransport({
 	service: 'gmail',
 	auth: {
-		user: process.env.GMAIL_ADD,
-		pass: process.env.GMAIL_PASS
+		user: 'mredwardcheever@gmail.com',
+		pass: 'Team77Rush',
 	},
 });
 
@@ -27,8 +27,8 @@ const stork = nodemailer.createTransport({
 app.post('/message', (req, res) => {
 	stork.sendMail(
 		{
-			from: process.env.GMAIL_ADD,
-			to: process.env.GMAIL_ADD,
+			from: 'mredwardcheever@gmail.com',
+			to: 'mredwardcheever@gmail.com',
 			subject: 'Message From Portfolio Page',
 			html: `From: ${req.body.email}\n\nMessage: ${req.body.message}`
 		},
