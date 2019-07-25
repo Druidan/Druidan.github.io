840cecd7abf34242bce060d84d462bb5402f1ed4
Druidan <44009123+Druidan@users.noreply.github.com>
2019-07-24 21:32:33 -0500

    Created the js file to hold my skills object.

---
+++ js/skills.js
@@ -0,0 +1,44 @@
+const skills ={
+    1: {
+        skill: 'HTML5',
+        pic: './assets/images/INSERTPICNAME.png',
+        link: '',
+        listClass: 'liHTML5',
+    },
+    2: {
+        skill: 'CSS3',
+        pic: './assets/images/INSERTPICNAME.png',
+        link: '',
+        listClass: 'liCSS3',
+    },
+    3: {
+        skill: '',
+        pic: './assets/images/INSERTPICNAME.png',
+        link: '',
+        listClass: 'li',
+    },
+    4: {
+        skill: '',
+        pic: './assets/images/INSERTPICNAME.png',
+        link: '',
+        listClass: 'li',
+    },
+    5: {
+        skill: '',
+        pic: './assets/images/INSERTPICNAME.png',
+        link: '',
+        listClass: 'li',
+    },
+    6: {
+        skill: '',
+        pic: './assets/images/INSERTPICNAME.png',
+        link: '',
+        listClass: 'li',
+    },
+    7: {
+        skill: '',
+        pic: './assets/images/INSERTPICNAME.png',
+        link: '',
+        listClass: 'li',
+    },
+}
