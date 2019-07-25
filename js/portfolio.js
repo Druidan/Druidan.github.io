c15b9386927fe46a2e89f66558b5a9408a4eb550
Druidan <44009123+Druidan@users.noreply.github.com>
2019-07-24 22:41:56 -0500

    I filled out my skills in the skills.js object, as well as the links to the images for those skills, and their list classes. Still need to add links, and create the function that builds the skills div below my biography page.

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,7 +1,7 @@
 const portfolio = {
     1: {
         title: 'Video Game Hangman',
-        pic: './assets/images/TVwide.png',
+        pic: './assets/images/portfolio/TVwide.png',
         description: 'Can you guess these video game titles?',
         deployed: 'https://druidan.github.io/VideoGameHangman/',
         repo: 'https://github.com/Druidan/VideoGameHangman', 
@@ -9,7 +9,7 @@ const portfolio = {
     },
     2:  {
         title: 'Star Wars Combat',
-        pic: './assets/images/SWLogo.png',
+        pic: './assets/images/portfolio/SWLogo.png',
         description: 'Take up your lightsaber and battle to be the last force-user left standing.',
         deployed: 'https://druidan.github.io/StarWarsCombat/',
         repo: 'https://github.com/Druidan/StarWarsCombat', 
@@ -17,7 +17,7 @@ const portfolio = {
     },
     3: {
         title: 'Whale Diplomacy',
-        pic: './assets/images/orcaCircles.gif',
+        pic: './assets/images/portfolio/orcaCircles.gif',
         description: 'The whales have come for revenge! Can you answer their trivia questions to prevent all out war?',
         deployed: 'https://druidan.github.io/WhaleDiplomacy/',
         repo: 'https://github.com/Druidan/WhaleDiplomacy', 
@@ -25,7 +25,7 @@ const portfolio = {
     },
     // 4: {
     //     title: 'Rock, Paper, Scissors Online',
-    //     pic: './assets/images/RPS.png',
+    //     pic: './assets/images/portfolio/RPS.png',
     //     description: 'Battle random strangers online in the school-yard classic game.',
     //     deployed: 'https://druidan.github.io/RPS-Multiplayer/index.html',
     //     repo: 'https://github.com/Druidan/RPS-Multiplayer.git', 
@@ -33,7 +33,7 @@ const portfolio = {
     // },
     // 5: {
     //     title: 'Jeopardy Training',
-    //     pic: './assets/images/JeopardyLogo.jpg',
+    //     pic: './assets/images/portfolio/JeopardyLogo.jpg',
     //     description: 'Train yourself for Jeopardy using these actual questions from the show!',
     //     deployed: 'https://druidan.github.io/JeopardyTraining/',
     //     repo: 'https://github.com/Druidan/JeopardyTraining', 
@@ -41,7 +41,7 @@ const portfolio = {
     // },
     6: {
         title: 'Liribot',
-        pic: './assets/images/LiriBot.png',
+        pic: './assets/images/portfolio/LiriBot.png',
         description: 'Have questions about movies, music, or local band shows? Ask Liribot!',
         videoWalkthrough: 'https://www.youtube.com/watch?v=blhtNukXXS4&feature=youtu.be',
         deployed: null,
@@ -50,7 +50,7 @@ const portfolio = {
     },
     // 7: {
     //     title: 'Bamazon',
-    //     pic: './assets/images/BamazonLogo.png',
+    //     pic: './assets/images/portfolio/BamazonLogo.png',
     //     description: 'Have questions about movies, music, or local band shows? Ask Liribot!',
     //     videoWalkthrough: 'https://www.youtube.com/watch?v=blhtNukXXS4&feature=youtu.be',
     //     deployed: null,
@@ -59,7 +59,7 @@ const portfolio = {
     // },
     8: {
         title: 'Nerd Friend Finder',
-        pic: './assets/images/nff.gif',
+        pic: './assets/images/portfolio/nff.gif',
         description: 'Ever had a hard time finding nerd friends who share your specific nerd tastes? Take our survey and find your closest match!',
         deployed: 'https://nerdfriendfinder.herokuapp.com/',
         repo: 'https://github.com/Druidan/NerdFinder', 
@@ -67,7 +67,7 @@ const portfolio = {
     },
     9: {
         title: 'Burger Muncher',
-        pic: './assets/images/muncher.png',
+        pic: './assets/images/portfolio/muncher.png',
         description: 'Keep track of each type of burger you munch with Burger Muncher!',
         deployed: 'https://druidansburgermuncher.herokuapp.com/',
         repo: 'https://github.com/Druidan/BurgerMuncher', 
@@ -75,7 +75,7 @@ const portfolio = {
     },
     10: {
         title: 'Burger Battle!',
-        pic: './assets/images/BurgerBattle.png',
+        pic: './assets/images/portfolio/BurgerBattle.png',
         description: 'Prepare for a king of the hill battle royale to decide which burger you want to eat right now.',
         deployed: 'https://burgerbattle.herokuapp.com/',
         repo: 'https://github.com/Druidan/BurgerBattle', 
@@ -83,7 +83,7 @@ const portfolio = {
     },
     11: {
         title: 'Game Mole',
-        pic: './assets/images/gMole2.png',
+        pic: './assets/images/portfolio/gMole2.png',
         description: 'When you need your gaming news fix, start at Game Mole - an aggregator of the latest news from the most popular game news sites on the internet.',
         deployed: 'https://gamemole.herokuapp.com/',
         repo: 'https://github.com/Druidan/GameMole', 
@@ -91,7 +91,7 @@ const portfolio = {
     },
     12: {
         title: 'Magical Memory',
-        pic: './assets/images/mMemory.png',
+        pic: './assets/images/portfolio/mMemory.png',
         description: 'A fan-made memory matching game based on Magic the Gathering, the popular competative strategy card game.',
         deployed: 'https://magicalmemory.herokuapp.com/',
         repo: 'https://github.com/Druidan/magicalMemory', 
@@ -99,7 +99,7 @@ const portfolio = {
     },
     13: {
         title: 'Fridge2Table',
-        pic: './assets/images/fridge2table_logo.png',
+        pic: './assets/images/portfolio/fridge2table_logo.png',
         description: 'Fridge2Table has one overarching goal - to make the modern problem of food management smooth and efficient. Fridge2Table gives users the ability to track their food wherever they store it with expiration dates and regular notifications if their food sits unused for too long.',
         deployed: 'https://play.google.com/store/apps/details?id=com.dreamteamdesigns.fridge2table',
         repo: 'https://github.com/Dreadpipe/fridge2table', 
